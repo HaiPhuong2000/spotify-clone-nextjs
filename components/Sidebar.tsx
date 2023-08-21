cc459f51662213e21f3279d674cd81e36712ca34
HaiPhuong2000 <nguyenhaiphuong2k@gmail.com>
2023-08-21 16:39:00 +0700

    update

---
+++ components/Sidebar.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Sidebar = () => {
+  return (
+    <div className="text-gray-500 px-5 pt-5 pb-36 text-xs lg:text-sm border-gray-900 h-screen overflow-y-scroll sm:max-w-[12rem] lg:max-w-[15rem] hidden md:block">
+      <div className="space-y-4"></div>
+    </div>
+  );
+};
