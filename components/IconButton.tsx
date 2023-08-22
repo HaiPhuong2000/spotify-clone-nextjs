f2e9c2641544936def0eee2f5d34df285aebc69f
HaiPhuong2000 <nguyenhaiphuong2k@gmail.com>
2023-08-22 17:54:27 +0700

    update

---
+++ components/IconButton.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface Props {
+  icon: (props: React.ComponentProps<'svg'>) => JSX.Element;
+  label: string;
+}
+
+export const IconButton = ({ icon: Icon, label }: Props) => {
+  return (
+    <button className="flex items-center space-x-2 hover:text-white">
+      <Icon className="h-5 w-5" />
+      <span>{label}</span>
+    </button>
+  );
+};
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
-
+import {
+  HomeIcon,
+  HeartIcon,
+  LibraryIcon,
+  PlusCircleIcon,
+  RssIcon,
+  SearchIcon,
+} from '@heroicons/react/outline';
+import { IconButton } from './IconButton';
 export const Sidebar = () => {
   return (
-    <div className="text-gray-500 px-5 pt-5 pb-36 text-xs lg:text-sm border-gray-900 h-screen overflow-y-scroll sm:max-w-[12rem] lg:max-w-[15rem] hidden md:block">
-      <div className="space-y-4"></div>
+    <div className="text-gray-500 px-5 pt-5 pb-36 text-xs lg:text-sm border-r border-gray-900 h-screen  sm:max-w-[12rem] lg:max-w-[15rem] hidden md:block">
+      <div className="space-y-4">
+        <IconButton icon={HomeIcon} label="Home" />
+        <IconButton icon={SearchIcon} label="Search" />
+        <IconButton icon={LibraryIcon} label="Your library" />
+        <IconButton icon={PlusCircleIcon} label="Create playlist" />
+        <IconButton icon={HeartIcon} label="Liked song" />
+        <IconButton icon={RssIcon} label="Your episodes" />
+      </div>
     </div>
   );
 };
